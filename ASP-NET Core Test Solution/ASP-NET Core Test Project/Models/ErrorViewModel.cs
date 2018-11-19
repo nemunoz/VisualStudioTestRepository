3d2e2750e59b766b6a60a20f42c660d4b0be6150
Néstor Fabio Muñoz García <nemunoz@outlook.com>
2018-11-19 20:02:00 +0100

    Proyecto vacío creado.

---
+++ ASP-NET Core Test Solution/ASP-NET Core Test Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP_NET_Core_Test_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
